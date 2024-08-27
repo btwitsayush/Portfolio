1366d4e694013787663fbc99ca933c90a35dc705
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-08-27 20:38:43 +0530

    Imported NavBar and render it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react"
+import NavBar from "./Components/NavBar"
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <NavBar />
     </>
   )
 }
