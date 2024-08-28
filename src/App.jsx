d18164f16959f23e924f30709ed74281664ba0b6
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-08-28 20:57:42 +0530

    Imported About Page component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import NavBar from "./Components/NavBar"
 import Home from "./Components/Home"
+import About from "./Components/About"
 function App() {
 
   return (
     <>
    <NavBar />
    <Home />
+   <About />
     </>
   )
 }
