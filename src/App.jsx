d3be437cca28f3295a33d244b50c0c9740da2ca3
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-08-28 21:32:54 +0530

    Imported Project Page component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import NavBar from "./Components/NavBar"
 import Home from "./Components/Home"
 import About from "./Components/About"
+import Projects from "./Components/Projects"
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
    <NavBar />
    <Home />
    <About />
+   <Projects />
     </>
   )
 }
