bb01981270dd238108b864038f4d13b0e9a8a498
Ayush Srivastava <srivastavaayush6393@gmail.com>
2025-04-23 23:09:12 +0530

    Created the footer section
    - Updated package.json and package-lock.json to use the latest versions of React, React DOM, React Router DOM, and added React Icons.
    - Improved the layout by adding a Footer component inside the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from './components/layout/Navbar'
 import Hero from './components/Hero'
+import Footer from './components/layout/Footer'
 // import './index.css'
 
 function App() {
   return (
-      <>
+    <>
       <Navbar />
       <Hero />
-      </>
-
+      <Footer />
+    </>
   )
 }
 
